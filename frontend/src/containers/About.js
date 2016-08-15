e8ac0fe96c18d172ecce05a753058a7836ab05f1
Nathan Smith <natesmith.copper@gmail.com>
2016-08-15 09:33:36 -0400

    syntax fix

--- frontend/src/containers/About.js
+++ frontend/src/containers/About.js
@@ -91,32 +91,12 @@ class About extends Component{
                 </Modal.Footer>
               </Modal>
               <div className='aboutContainer1'>
-              <img className='natePic'/><br></br>
-                <Button className='button' bsStyle="danger" onClick={() => this.openModal('nate')}>
-                    About Nate
-            </Button>
+                <img className='natePic'/><br></br>
+                  <Button className='button' bsStyle="danger" onClick={() => this.openModal('nate')}>
+                      About Nate
+                  </Button>
+              </div>
           </div>
-
-        <div className="wrapper">
-        <div className="infoCard">
-           <Modal show={this.state.showModalCarrow} onHide={() => this.closeModal('carrow')}>
-              <Modal.Header closeButton>
-                <Modal.Title className='title'>Carrow Thibault</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <p>{Profiles.carrowText()}</p>
-              </Modal.Body>
-              <Modal.Footer>
-                <Button bsStyle='primary' onClick={(name) => this.closeModal('carrow')}>Close</Button>
-              </Modal.Footer>
-            </Modal>
-            <div className='aboutContainer1'>
-              <img className='carrowPic'/><br></br>
-              <Button className='button' bsStyle="danger" onClick={() => this.openModal('carrow')}>
-                  About Carrow
-              </Button>
-            </div>
-        </div>
 
           <div className="infoCard">
             <Modal show={this.state.showModalBaba} onHide={() => this.closeModal('baba')}>
